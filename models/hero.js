1a33d822e5eb5c9069fc31fc2cdd49b20acb9edb
nisal jayathilaka <nisaljayathilaka@nisals-MacBook-Air.local>
2020-03-04 12:59:19 +0530

    nisal

--- models/hero.js
+++ models/hero.js
@@ -21,7 +21,7 @@ const heroSchema = new mongoose.Schema({
     superPowers : {
         required: true,
         type:[String],
-        enum : ['Invisibility','Barking','Flying','runnig','walking','lol'],
+        enum : ['Invisibility','Flying','runnig','walking','lol',',jkjm'],
         default:undefined
     },
     moview : [String]
